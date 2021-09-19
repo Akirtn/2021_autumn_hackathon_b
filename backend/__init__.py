c22f065b54f7b79c9c1bfe075fde7c66d8b79f74
STU-Idichi-Syoya <e1q19006@oit.ac.jp>
2021-09-19 11:47:12 +0900

    f

--- backend/__init__.py
+++ backend/__init__.py
@@ -53,7 +53,7 @@ def create_app():
 
     @app.after_request
     def after_request(response):
-        response.headers.add('Access-Control-Allow-Origin', '*')
+        # response.headers.add('Access-Control-Allow-Origin', '*')
         # response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
         # response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
         return response

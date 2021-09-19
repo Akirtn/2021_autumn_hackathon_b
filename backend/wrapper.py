4cdcb8f7164a3ca6582a8a2187580ac70e746b78
MiyabiTane <miyabitane@yahoo.co.jp>
2021-09-19 16:26:01 +0900

    fix

--- backend/wrapper.py
+++ backend/wrapper.py
@@ -158,7 +158,7 @@ def find_matched_result(user_info):
         input_dic[str(user_id)] = {"xxx_times": []}
         for empty_schedule in empty_schedules:
             input_dic[str(user_id)]["xxx_times"].append(
-                {"start_at": empty_schedule.start_time, "ends_at": empty_schedule.end_time})
+                {"starts_at": empty_schedule.start_time, "ends_at": empty_schedule.end_time})
     print(input_dic)
     print(user_info.id)
     matched_users, matched_start_time = calc_empty_schedule_humans(user_info.id, input_dic)

605bd42b1beb95659da3fc50b55777aac64ec5e8
STU-Idichi-Syoya <e1q19006@oit.ac.jp>
2021-09-19 11:34:26 +0900

    fix

--- backend/__init__.py
+++ backend/__init__.py
@@ -50,4 +50,12 @@ def create_app():
     app.register_blueprint(schdule_blueprint)
 
     CORS(app, resources={"/*": {"origins": "*"}})
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        return response
+
     return app

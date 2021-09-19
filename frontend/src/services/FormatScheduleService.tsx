ee10bfe490a87971cda027be45557eebf32f8941
ayoshida1101 <aukt1011161@icloud.com>
2021-09-19 11:48:34 +0900

    feature: schedules を fullCalendar に合うように整形する Service を作成

---
+++ frontend/src/services/FormatScheduleService.tsx
@@ -0,0 +1,45 @@
+import { DateDomainService } from './DateDomainService'
+import {
+   MatchedSchedule,
+   MatchedSchedules,
+   EmptySchedule,
+   EmptySchedulesType,
+} from '../mocks/fetchData'
+import Theme from '../utils/theme'
+
+export class FormatScheduleService {
+   //emptySchedules を fullCalendar に合う形で整形するメソッド
+   public getFormattedEmptySchedules(emptySchedules: EmptySchedulesType): any {
+      const dateDomainService = new DateDomainService()
+      return emptySchedules.map((schedule: EmptySchedule) => {
+         return {
+            title: '空き時間',
+            start: dateDomainService.changeUnixTimeToFormattedString(
+               schedule.start_at
+            ),
+            end: dateDomainService.changeUnixTimeToFormattedString(
+               schedule.end_at
+            ),
+            color: Theme.color.primaryColor,
+         }
+      })
+   }
+   //matchedSchedules を fullCalendar に合う形で整形するメソッド
+   public getFormattedMatchedSchedules(
+      matchedSchedules: MatchedSchedules
+   ): any {
+      const dateDomainService = new DateDomainService()
+      return matchedSchedules.map((schedule: MatchedSchedule) => {
+         return {
+            title: schedule.matched_member.name,
+            start: dateDomainService.changeUnixTimeToFormattedString(
+               schedule.start_at
+            ),
+            end: dateDomainService.changeUnixTimeToFormattedString(
+               schedule.end_at
+            ),
+            color: Theme.color.secondaryColor,
+         }
+      })
+   }
+}
